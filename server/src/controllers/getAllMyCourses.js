e0441f443e67e1f4e3f6a2412b98dbfd1a4242f2
patrickmurayari <patrickmurayari03@gmail.com>
2023-10-09 20:41:37 -0300

    add user with courses

--- server/src/controllers/getAllMyCourses.js
+++ server/src/controllers/getAllMyCourses.js
@@ -1,11 +1,32 @@
 
-const { User ,Curso } = require('../db');
-const getmycourses = ({email}) => {
+const { User, Curso, Usuario_Curso } = require('../db');
+const getmycourses = async ({ email }) => {
     try {
-        
+        const userDb = await User.findOne({
+            where: { email: email }
+        })
+
+        if (userDb && userDb.isDeleted) {
+            return userDb.isDeleted
+        }
+
+        const cursoDb = await Usuario_Curso.findAll({
+            where: { UserId: userDb.id }
+        })
+
+        const cursoIds = cursoDb.map((curso) => curso.CursoId);
+
+        const CursosAll = await Curso.findAll({
+            where: { id: cursoIds }
+        })
+
+        console.log(CursosAll, 'cursosAll');
+
+        return CursosAll
+
     } catch (error) {
-        
+        throw new Error(error)
     }
 }
 
-module.exports=getmycourses
+module.exports = getmycourses
