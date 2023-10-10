e6c6b809a3de95e1165f4e8d1a61c0f7045132c0
patrickmurayari <patrickmurayari03@gmail.com>
2023-10-10 15:58:04 -0300

    add back papa

--- server/src/app.js
+++ server/src/app.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Configuración CORS específica
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
