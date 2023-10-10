f3bc028127f80fac46be5d951e1a5aef542c9d0a
joak990 <joakhaidar@gmail.com>
2023-10-10 20:09:32 -0300

    new

--- server/src/models/UsuariosModels.js
+++ server/src/models/UsuariosModels.js
@@ -1,41 +1,57 @@
 const { DataTypes } = require('sequelize');
 
+function generateOTP() {
+  return Math.floor(1000 + Math.random() * 9000).toString(); // Genera un número aleatorio de 4 dígitos
+}
+
 module.exports = (sequelize) => {
-    sequelize.define("User", {
-        id: {
-            type: DataTypes.STRING,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false,
-        },
-        uid: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        type: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        root: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        isDeleted: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        }, 
-    });
-};
+  sequelize.define("User", {
+    id: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    uid: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    root: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    otp: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        is: /^[0-9]{4}$/ // Valida que tenga exactamente 4 dígitos
+      }
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
+
+  // Genera un código OTP de 4 dígitos al crear un nuevo usuario
+  sequelize.addHook('beforeCreate', (user) => {
+    user.otp = generateOTP();
+  });
+}
