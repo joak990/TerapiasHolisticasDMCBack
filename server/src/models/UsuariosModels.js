045ce6d16115b1ec17cc24a8a4e1d03e681d199a
joak990 <joakhaidar@gmail.com>
2023-10-10 20:03:46 -0300

    otp

--- server/src/models/UsuariosModels.js
+++ server/src/models/UsuariosModels.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid'); // Importa la función uuidv4
 
 module.exports = (sequelize) => {
     sequelize.define("User", {
@@ -32,10 +33,22 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        otp: {
+            type: DataTypes.STRING, // Puedes usar STRING para el código OTP
+            allowNull: true,
+            validate: {
+                is: /^[0-9]{4}$/ // Valida que tenga exactamente 4 dígitos
+            }
+        },
         isDeleted: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: false
         },
+    });
+
+    // Genera un código OTP de 4 dígitos al crear un nuevo usuario
+    sequelize.addHook('beforeCreate', (user) => {
+        user.otp = Math.floor(1000 + Math.random() * 9000).toString(); // Genera un número aleatorio de 4 dígitos
     });
 };
