cb4ad0e1bbc5922d86568a6d571c2fba6f0ff1e3
joak990 <joakhaidar@gmail.com>
2023-10-11 20:37:45 -0300

    quitar tarj/cred

--- server/src/routes/mercadopago.js
+++ server/src/routes/mercadopago.js
@@ -1,45 +1,46 @@
-
 const express = require('express');
 const mercadoPagoRouter = express.Router()
 const mercadopago = require('mercadopago')
 
-
-
-
 mercadoPagoRouter.post("/mercado_pago",  async (req, res) => {
 
-    try {
-        //- Configurar la preferencia de MercadoPago
-    
-      
-    
-        const preference = {
-          items: [
-            {
-           
-              title: req.body.title,
-              description: "cosa para comprar",
-              unit_price: req.body.price,
-              currency_id: "ARS", // 
-              quantity: req.body.quantity, 
-            }, 
-          ],
-          back_urls: {
-            "success": "http://localhost:5173/",
-            "failure": "http://localhost:5173/error",
-            "pending": ""
-          },
-          auto_return: "approved",
-        };
-    
-        const response = await mercadopago.preferences.create(preference);
-        res.status(200).json({id :response.body.id });
-      } catch (error) {
-        console.error("Error al procesar la solicitud:", error);
-        res.status(500).send(error.message);
+  try {
+    // Configurar la preferencia de MercadoPago
+    const preference = {
+      items: [
+        {
+          title: req.body.title,
+          description: "cosa para comprar",
+          unit_price: req.body.price,
+          currency_id: "ARS",
+          quantity: req.body.quantity,
+        },
+      ],
+      back_urls: {
+        "success": "http://localhost:5173/",
+        "failure": "http://localhost:5173/error",
+        "pending": ""
+      },
+      auto_return: "approved",
+      payment_methods: {
+        excluded_payment_methods: [],
+        excluded_payment_types: [
+        
+                  {
+                            id: "credit_card"
+                  },
+                  {
+                    id: "ticket"
+                  }]
       }
+    };
 
+    const response = await mercadopago.preferences.create(preference);
+    res.status(200).json({ id: response.body.id });
+  } catch (error) {
+    console.error("Error al procesar la solicitud:", error);
+    res.status(500).send(error.message);
+  }
+});
 
-})
-
-module.exports = mercadoPagoRouter
+module.exports = mercadoPagoRouter;
