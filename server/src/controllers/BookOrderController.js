7e5400ba21b63c691bf7a9b9824b773248267c6e
joak990 <joakhaidar@gmail.com>
2024-01-17 11:34:07 -0300

    ruta nodemailer

---
+++ server/src/controllers/BookOrderController.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+const sendbookorder = async (data) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'patrickmurayari03@gmail.com',
+                pass: 'okzbxjuqdtdubzti'
+            },
+        });
+
+        const htmlMessage = `
+            <div style="max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+                <h1 style="color: #333; font-size: 24px;">¡Pedido de compra!</h1>
+                <p style="margin: 10px 0; color: #555;">Nombre: ${data.nombre}</p>
+                <p style="margin: 10px 0; color: #555;">Apellido: ${data.apellido}</p>
+                <p style="margin: 10px 0; color: #555;">Email: ${data.email}</p>
+                <p style="margin: 10px 0; color: #555;">Teléfono: ${data.telefono}</p>
+            </div>
+        `;
+
+        const info = await transporter.sendMail({
+            from: 'patrickmurayari03@gmail.com',
+            to: 'joakhaidar@gmail.com',
+            subject: '¡Pedido de Compra!',
+            html: htmlMessage
+        });
+     
+        console.log('Correo enviado con éxito:', info.response);
+        return true;
+    } catch (error) {
+        console.error('Error al enviar el correo:', error.message);
+        return false;
+    }
+}
+
+module.exports = sendbookorder;
--- server/src/routes/nodeMailer.js
+++ server/src/routes/nodeMailer.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const Mailerrouter = express.Router()
 
-const emailController = require('../controllers/NodeMailer.js')
+const emailController = require('../controllers/NodeMailer.js');
+const sendbookorder = require('../controllers/BookOrderController');
 
 Mailerrouter.post('/',async(req,res ) => {
     try {
         const {email} = req.body
         const emailUser = await emailController({email})
         res.status(200).json(emailUser);
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
+})
+
+
+Mailerrouter.post('/book',async(req,res ) => {
+    try {
+        const {nombre,apellido,email,telefono} = req.body
+    const response = await sendbookorder({nombre,apellido,email,telefono})
+        res.status(200).json(response);
     } catch (error) {
         res.status(400).send({ error: error.message })
     }
