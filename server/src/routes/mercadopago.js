89a9588059f14ac70be9e8835035de2869d4f94c
joak990 <joakhaidar@gmail.com>
2024-02-06 21:35:10 -0300

    return success

--- server/src/routes/mercadopago.js
+++ server/src/routes/mercadopago.js
@@ -17,8 +17,8 @@ mercadoPagoRouter.post("/mercado_pago",  async (req, res) => {
         },
       ],
       back_urls: {
-        "success": "https://amazing-buttercream-47d324.netlify.app/success",
-        "failure": "https://amazing-buttercream-47d324.netlify.app/error",
+        "success": "https://terapiasholisticasdmc.com.ar/success",
+        "failure": "https://terapiasholisticasdmc.com.ar/error",
         "pending": ""
       },
       auto_return: "approved",
